ace8b02f19860b10ff4b13909635cef38a16900b
Daniel Eden <dan.eden@me.com>
2020-12-07 11:00:17 +0000

    Initial Commit

---
+++ Muffler/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Muffler
+//
+//  Created by Daniel Eden on 07/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
