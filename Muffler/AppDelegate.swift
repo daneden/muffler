49df8379293cc524cd82aa9fd2983c9ab68741e7
Tony Metzidis <anthony.metzidis@gmail.com>
2020-12-07 10:27:11 -0800

    add global mic mute
    added global mic mute to applescript

--- Muffler/AppDelegate.swift
+++ Muffler/AppDelegate.swift
@@ -69,6 +69,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         set toggledMute to true
       end tell
     end if
+                                   
+    set inputVolume to input volume of (get volume settings)
+    if inputVolume = 0 then
+      set inputVolume to 100
+    else
+    set inputVolume to 0
+      end if
+    set volume input volume inputVolume                           
 
     if toggledMute is false then
       display notification "No mutable application found"
