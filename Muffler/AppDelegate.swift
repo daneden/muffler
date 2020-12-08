bd9ca97c27f82590f537cea0e67229615c1b25e4
Daniel Eden <dan.eden@me.com>
2020-12-08 16:11:43 +0000

    Allow app to run on macOS 10.15 and refactor toggleMute function

--- Muffler/AppDelegate.swift
+++ Muffler/AppDelegate.swift
@@ -8,14 +8,25 @@
 import Cocoa
 import SwiftUI
 
+enum MuteKind {
+  case video, audio
+}
+
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
   var statusItem: NSStatusItem!
   var statusItemMenu: NSMenu!
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
+    var image: NSImage
     statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    let image = NSImage(systemSymbolName: "mic.slash.fill", accessibilityDescription: "Mute Video or Microphone")!
+    
+    if #available(OSX 11.0, *) {
+      image = NSImage(systemSymbolName: "mic.slash.fill", accessibilityDescription: "Mute Video or Microphone")!
+    } else {
+      image = NSImage(named: "mic")!
+    }
+    
     statusItem.button?.image = image
 
     statusItemMenu = .init()
@@ -30,7 +41,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     NSApplication.shared.terminate(self)
   }
   
-  @objc func toggleMuteMic(_ sender: Any?) {
+  func toggleMute(ofKind muteKind: MuteKind) {
+    var bluejeansKey: String
+    var verb: [String]
+    var noun: String
+    var zoomMenuItemTitles: [String]
+    
+    switch muteKind {
+    case .audio:
+      bluejeansKey = "m"
+      verb = ["Muted", "Unmuted"]
+      noun = "audio"
+      zoomMenuItemTitles = ["Mute Audio", "Unmute Audio"]
+    case .video:
+      bluejeansKey = "v"
+      verb = ["Stopped", "Started"]
+      noun = "video"
+      zoomMenuItemTitles = ["Stop Video", "Start Video"]
+    }
+    
     let script = NSAppleScript(source: """
     set bluejeans to "BlueJeans"
     set zoomus to "zoom.us"
@@ -46,10 +75,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
           if not frontmost of process bluejeans then
             set frontmost of process bluejeans to true
           end if
-          keystroke "m"
+          keystroke "\(bluejeansKey)"
           set frontmost of process activeApp to true
           
-          display notification "Toggled mute for BlueJeans"
+          display notification "Toggled \(noun) mute for BlueJeans"
           set toggledMute to true
         end if
       end tell
@@ -57,15 +86,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     if application zoomus is running then
       tell application "System Events" to tell process zoomus
-        if menu item "Unmute Audio" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
-          click menu item "Unmute Audio" of menu 1 of menu bar item "Meeting" of menu bar 1
-          set verb to "Unmuted"
-        else if menu item "Mute Audio" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
-          click menu item "Mute Audio" of menu 1 of menu bar item "Meeting" of menu bar 1
-          set verb to "Muted"
+        if menu item "\(zoomMenuItemTitles[1])" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
+          click menu item "\(zoomMenuItemTitles[1])" of menu 1 of menu bar item "Meeting" of menu bar 1
+          set verb to "\(verb[1])"
+        else if menu item "\(zoomMenuItemTitles[0])" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
+          click menu item "\(zoomMenuItemTitles[0])" of menu 1 of menu bar item "Meeting" of menu bar 1
+          set verb to "\(verb[0])"
         end if
         
-        display notification verb & " microphone on Zoom"
+        display notification verb & " \(noun) on Zoom"
         set toggledMute to true
       end tell
     end if
@@ -83,57 +112,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
   }
   
+  @objc func toggleMuteMic(_ sender: Any?) {
+    toggleMute(ofKind: .audio)
+  }
+  
   @objc func toggleMuteVideo(_ sender: Any?) {
-    let script = NSAppleScript(source: """
-    set bluejeans to "BlueJeans"
-    set zoomus to "zoom.us"
-    set toggledMute to false
-
-    tell application "System Events"
-      set activeApp to name of first application process whose frontmost is true
-    end tell
-
-    if application bluejeans is running then
-      tell application "System Events"
-        if (exists process bluejeans) then
-          if not frontmost of process bluejeans then
-            set frontmost of process bluejeans to true
-          end if
-          keystroke "v"
-          set frontmost of process activeApp to true
-          
-          display notification "Toggled video for BlueJeans"
-          set toggledMute to true
-        end if
-      end tell
-    end if
-
-    if application zoomus is running then
-      tell application "System Events" to tell process zoomus
-        if menu item "Start Video" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
-          click menu item "Start Video" of menu 1 of menu bar item "Meeting" of menu bar 1
-          set verb to "Started"
-        else if menu item "Stop Video" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
-          click menu item "Stop Video" of menu 1 of menu bar item "Meeting" of menu bar 1
-          set verb to "Stopped"
-        end if
-        
-        display notification verb & " video on Zoom"
-        set toggledMute to true
-      end tell
-    end if
-
-    if toggledMute is false then
-      display notification "No mutable application found"
-    end if
-    """)
-    
-    var error: NSDictionary?
-    if let returnDescription = script?.executeAndReturnError(&error).stringValue {
-      print(returnDescription)
-    } else if error != nil {
-      print(error?.description ?? "Error")
-    }
+    toggleMute(ofKind: .video)
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
